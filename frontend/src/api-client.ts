db87ba8111bea6d16c2de2dd85a520a75b227d86
Chris Blakely <chrisblakely01@users.noreply.github.com>
2023-11-29 18:28:35 +0000

    fixing types

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,7 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
-import { HotelType } from "../../backend/shared/types";
-
+import { HotelType } from "../../backend/src/shared/types";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const register = async (formData: RegisterFormData) => {
