740d565913f35dbba7fedd660c16f61eb4ae8f38
Chris Blakely <chrisblakely01@users.noreply.github.com>
2023-11-29 18:14:42 +0000

    adding shared folder

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 import cloudinary from "cloudinary";
-import Hotel, { HotelType } from "../models/hotel";
+import Hotel from "../models/hotel";
 import verifyToken from "../middleware/auth";
 import { body } from "express-validator";
+import { HotelType } from "../../../shared/types";
 
 const router = express.Router();
 
