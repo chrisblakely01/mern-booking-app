18915a2837aa6c5fd3d824141114cccc8188ea46
Chris Blakely <chrisblakely01@users.noreply.github.com>
2023-11-29 14:29:32 +0000

    fixing routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,6 +34,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(7000, () => {
   console.log("server running on localhost:7000");
 });
