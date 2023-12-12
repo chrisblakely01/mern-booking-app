9560276ea57c1b40def83b7c9dbd54c377a993c0
Chris Blakely <chrisblakely01@users.noreply.github.com>
2023-12-12 14:41:14 +0000

    fixing build and typescript errors

--- frontend/src/components/FacilitiesFilter.tsx
+++ frontend/src/components/FacilitiesFilter.tsx
@@ -1,4 +1,4 @@
-import { hotelFacilities, hotelTypes } from "../config/hotel-options-config";
+import { hotelFacilities } from "../config/hotel-options-config";
 
 type Props = {
   selectedFacilities: string[];
