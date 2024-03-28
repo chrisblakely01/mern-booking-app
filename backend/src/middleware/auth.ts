8032087542c6ff6cd075ae359df5858d2941aef1
nada <nadabentaher2003@gmail.com>
2024-03-28 02:02:43 +0100

    first commit

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
+// Déclaration pour étendre l'interface Request du module Express
 declare global {
   namespace Express {
     interface Request {
@@ -9,19 +10,26 @@ declare global {
   }
 }
 
+
+// Middleware(fct qui s'exécute entre la réception d'une requête et l'envoi d'une réponse) pour vérifier le token d'authentification
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+   // Récupère le token d'authentification à partir des cookies de la requête
   const token = req.cookies["auth_token"];
+  // Vérifie si le token n'est pas présent
   if (!token) {
     return res.status(401).json({ message: "unauthorized" });
   }
 
   try {
+     // Verifies the validity of the token using the JWT secret key
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    // Extrait l'id du user à partir des informations décodées du token
     req.userId = (decoded as JwtPayload).userId;
+    // Calls the next function in the middleware chain
     next();
   } catch (error) {
     return res.status(401).json({ message: "unauthorized" });
   }
 };
 
-export default verifyToken;
+export default verifyToken;// Exporte la fonction verifyToken comme middleware
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,8 +5,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import verifyToken from "../middleware/auth";
 
-const router = express.Router();
-
+const router = express.Router();// Création d'un routeur express
+// Route for user login
 router.post(
   "/login",
   [
@@ -21,19 +21,22 @@ router.post(
       return res.status(400).json({ message: errors.array() });
     }
 
-    const { email, password } = req.body;
+    const { email, password } = req.body;// Extraction de l'email et du mot de passe de la requête
 
     try {
+      // Find user by email
       const user = await User.findOne({ email });
+       // If user not found, return error
       if (!user) {
         return res.status(400).json({ message: "Invalid Credentials" });
       }
-
+      // Compare password with hashed password stored in database
       const isMatch = await bcrypt.compare(password, user.password);
+      // If passwords don't match, return error
       if (!isMatch) {
         return res.status(400).json({ message: "Invalid Credentials" });
       }
-
+      // Generate JWT token
       const token = jwt.sign(
         { userId: user.id },
         process.env.JWT_SECRET_KEY as string,
@@ -47,6 +50,7 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
       });
+      // Return user ID in response
       res.status(200).json({ userId: user._id });
     } catch (error) {
       console.log(error);
@@ -54,12 +58,15 @@ router.post(
     }
   }
 );
-
+// Route to validate JWT token
 router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+  // Return user ID extracted from token
   res.status(200).send({ userId: req.userId });
 });
 
+// Route to logout (clear authentication token)
 router.post("/logout", (req: Request, res: Response) => {
+  // Effacer authentication token en le définissant comme une chaîne vide et en l'expirant
   res.cookie("auth_token", "", {
     expires: new Date(0),
   });
